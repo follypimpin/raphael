64ac8f1850fa6158808fda4ea72eb0c66f248a32
Raphael <rolanrewaju@fcptech.com>
2018-05-06 00:54:15 +0100

    Database Migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,19 +7,22 @@
                 1 of 2
             </div>
             <div class="col">
-               <h1>Home</h1>
+                <h1>Home</h1>
             </div>
         </div>
         <div class="row">
             <div class="jumbotron">
                 <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
+                    attention to featured content or information.</p>
                 <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p>It uses utility classes for typography and spacing to space content out within the larger
+                    container.</p>
                 <p class="lead">
                     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </p>
             </div>
         </div>
+
     </div>
 @endsection
